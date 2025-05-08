88c14fab2a3804f95db7349a511817d4e26e7827
Rewired6985 <atticusjagodzinski@gmail.com>
2025-05-08 18:20:30 -0500

    % Second attempt at initial commit

---
+++ tank2025/src/main/java/frc/robot/m_timer.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class m_timer {
+
+    public static void restart() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'restart'");
+    }
+
+    public static double get() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+}
